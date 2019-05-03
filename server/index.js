64dc890ce2bfb3139438f9f43128ed9130b380be
Jordis Man <jordisman@gmail.com>
2019-05-03 12:51:55 -0700

    edit for EC2

--- server/index.js
+++ server/index.js
@@ -11,10 +11,10 @@ app.use(cors());
 app.use(morgan('dev'));
 
 const staticPath = `${__dirname}/../public`;
-app.use('/books/:id', express.static(staticPath));
+app.use('/booksInfo/:id', express.static(staticPath));
 
-app.use('/books/:id', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
-app.use('/books', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
+app.use('/books/:id', proxy({ target: 'http://ec2-52-53-235-189.us-west-1.compute.amazonaws.com:3001', changeOrigin: true }));
+// app.use('/books', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
 
 // hannah-service
 // app.use(
@@ -39,3 +39,28 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+
+// const staticPath = `${__dirname}/../public`;
+// app.use('/booksInfo/:id', express.static(staticPath));
+
+// app.use('/books/:id', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
+// app.use('/books', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
+
+// hannah-service
+// app.use(
+//   '/books/:id/reviews',
+//   proxy({ target: 'http://localhost:3003', changeOrigin: true }),
+// );
+
+// kaz-service
+// app.use(
+//   '/books/:id/info',
+//   proxy({ target: 'http://localhost:3002', changeOrigin: true }),
+// );
+
+// ginger-service
+// app.use(
+//   '/books/:id/authors',
+//   proxy({ target: 'http://localhost:3000', changeOrigin: true }),
+// );
